114bf7d162319d6eaf467751de35ec2cbf9a18b0
alex <alex.m.larson@gmail.com>
2015-10-20 09:36:08 -0700

    be my final draft v2

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,35 @@
+$(document).ready(function() {
 
-$(document).ready(function (){
+    $("#click").click(function() {
 
-$("#click").click(function (){
+        $("div.number").empty();
 
-  $("div.number").empty();
+        var num = $("input#num").val();
 
-  var num = $("input#num").val();
+        for (var start = 1; start <= num; start += 1) {
 
-  for (var start = 1; start <= num; start += 1) {
+            if (start % 15 === 0) {
+                $("div.number").append("<li>Ping-Pong</li>");
 
-      if (start % 15 === 0) {
-          $("div.number").append("<li>Ping-Pong</li>");
+            } else if (start % 3 === 0) {
+                $("div.number").append("<li>Ping</li>");
 
-      } else if (start % 3 === 0) {
-          $("div.number").append("<li>Ping</li>");
-
-      } else if (start % 5 === 0) {
-          $("div.number").append("<li>Pong</li>");
-
-
-      } else  {
-          $("div.number").append("<li>" + start + "</li>");
-
-          event.preventDefault();
+            } else if (start % 5 === 0) {
+                $("div.number").append("<li>Pong</li>");
 
 
-      }
+            } else {
+                $("div.number").append("<li>" + start + "</li>");
 
-  }
+                event.preventDefault();
 
 
-})
+            }
+
+        }
+
+
+    })
 
 
 })
